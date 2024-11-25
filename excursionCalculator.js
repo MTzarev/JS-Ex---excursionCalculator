3df73fa9cde396020bc21366e222ddd23cd8f1c3
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-25 23:31:22 +0200

    Update excursionCalculator.js

--- excursionCalculator.js
+++ excursionCalculator.js
@@ -1,7 +1,7 @@
 function excursionCalculator(input) {
     let touristNum = Number(input[0]);
     let season = input[1];
-    let price = 0
+    let price = 0;
     if (touristNum > 0 && touristNum <= 5) {
         if (season === "spring") {
             price = touristNum * 50
@@ -18,17 +18,17 @@ function excursionCalculator(input) {
         } else if (season === "summer") {
             price = touristNum * 45;
         } else if (season === "autumn") {
-            price = touristNum * 49.5
+            price = touristNum * 49.5;
         } else if (season === "winter") {
-            price = touristNum * 85
+            price = touristNum * 85;
         }
     }
     if (season === "summer") {
-        price = price * 0.85
+        price = price * 0.85;
     } else if (season === "winter") {
-        price = price * 1.08
+        price = price * 1.08;
     } else {
-        price = price
+        price = price;
     }
 
     console.log(`${price.toFixed(2)} leva.`);
